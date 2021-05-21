2f2c86727e0feb0338eaacd00eed6e57d87ab260
Oever González <software@notengobattery.com>
2021-05-20 22:37:18 -0600

    custom: general modifications to OpenWrt
    These modifications include:
      - Optimizing the compression parameters
      - Enable extra kernel modules
      - Remove `zram` package from `system`
      - Enable wireless by default
      - Apply fixes for broken packages
    
    Signed-off-by: Oever González <software@notengobattery.com>

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -105,19 +105,20 @@ detect_mac80211() {
 
 		uci -q batch <<-EOF
 			set wireless.radio${devidx}=wifi-device
-			set wireless.radio${devidx}.type=mac80211
 			set wireless.radio${devidx}.channel=${channel}
+			set wireless.radio${devidx}.disabled=0
 			set wireless.radio${devidx}.hwmode=11${mode_band}
+			set wireless.radio${devidx}.type=mac80211
 			${dev_id}
 			${ht_capab}
-			set wireless.radio${devidx}.disabled=1
 
 			set wireless.default_radio${devidx}=wifi-iface
 			set wireless.default_radio${devidx}.device=radio${devidx}
-			set wireless.default_radio${devidx}.network=lan
+			set wireless.default_radio${devidx}.encryption=sae-mixed
+			set wireless.default_radio${devidx}.key=NoTengoBattery
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio${devidx}.network=lan
+			set wireless.default_radio${devidx}.ssid=OpenWrt-NoTengoBattery
 EOF
 		uci -q commit wireless
 
