ebdc1a27dd093e4951c98446ead44be50fb53397
Oever González <software@notengobattery.com>
2021-09-13 20:42:09 -0600

    custom: improve the seeds design to allow slim builds
    Signed-off-by: Oever González <software@notengobattery.com>

--- build_me.pl
+++ build_me.pl
@@ -41,24 +41,27 @@ my ($supportURL) =
   . catdir( ROOT_URL, PROJECTS, PROJECT_NAME, RELEASES, VERSION,
     RELEASE_NOTES );
 my ($curDir)            = curdir();
-my ($dSeed)             = catdir( glob($curDir), 'seeds' );
-my ($dScripts)          = catdir( glob($curDir), 'scripts' );
-my ($linuxTarget)       = catdir( glob($curDir), LINUX_TARGET );
+my ($dSeed)             = catdir( glob($curDir),    'seeds' );
+my ($dScripts)          = catdir( glob($curDir),    'scripts' );
+my ($linuxTarget)       = catdir( glob($curDir),    LINUX_TARGET );
 my ($fConfig)           = catfile( glob($curDir),   '.config' );
 my ($fConfigSeed)       = catfile( glob($curDir),   'config.seed' );
 my ($fCommonSeed)       = catfile( glob($dSeed),    'common.seed' );
-my ($fKernelSeed)       = catfile( glob($dSeed),    'kernel.seed' );
-my ($fPackagesSeed)     = catfile( glob($dSeed),    'packages.seed' );
-my ($fTargetSeed)       = catfile( glob($dSeed),    TARGET . '.seed' );
-my ($fSubTargetSeed)    = catfile( glob($dSeed),    TARGET . '-' . SUBTARGET . '.seed' );
+my ($fFeaturesSeed)     = catfile( glob($dSeed),    'features.seed' );
 my ($fKernelCommonSeed) = catfile( glob($dSeed),    'common-kernel.seed' );
+my ($fKernelSeed)       = catfile( glob($dSeed),    'kernel.seed' );
 my ($fKernelTargetSeed) = catfile( glob($dSeed),    TARGET . '-kernel.seed' );
+my ($fPackagesSeed)     = catfile( glob($dSeed),    'packages.seed' );
+my ($fSubTargetSeed)    = catfile( glob($dSeed),    TARGET . '-' . SUBTARGET . '.seed' );
+my ($fTargetSeed)       = catfile( glob($dSeed),    TARGET . '.seed' );
 my ($fScriptDiff)       = catfile( glob($dScripts), 'diffconfig.sh' );
 my ($kconfg) = readpipe( sprintf "find %s -type f -path '*/%s/*config-*.*'",
     $linuxTarget, TARGET );
 
 open( COMMON_SEED, "<", glob($fCommonSeed) )
   or die qq(Could not open file '$fCommonSeed' : $!);
+open( FEATURES_SEED, "<", glob($fFeaturesSeed) )
+  or die qq(Could not open file '$fFeaturesSeed' : $!);
 open( KERNEL_SEED, "<", glob($fKernelSeed) )
   or die qq(Could not open file '$fKernelSeed' : $!);
 open( TARGET_SEED, "<", glob($fTargetSeed) )
@@ -78,17 +81,18 @@ open( CONFIG, ">", glob($fConfig) )
 open( KCONFIG, ">>", glob($kconfg) )
   or die qq(Could not open file '$kconfg' : $!);
 
-printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_BUG_URL",  glob($issuesURL) );
-printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_DIST",     DIST );
-printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_HOME_URL", glob($releaseURL) );
-printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_NUMBER",   VERSION );
-printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_REPO",     glob($downloadURL) );
+printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_BUG_URL",     glob($issuesURL) );
+printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_DIST",        DIST );
+printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_HOME_URL",    glob($releaseURL) );
+printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_NUMBER",      VERSION );
+printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_REPO",        glob($downloadURL) );
 printf( CONFIG "%s=\"%s\"\n", "CONFIG_VERSION_SUPPORT_URL", glob($supportURL) );
-print( CONFIG <COMMON_SEED>,  "\n" );
-print( CONFIG <KERNEL_SEED>,  "\n" );
-print( CONFIG <TARGET_SEED>,  "\n" );
-print( CONFIG <SUBTARGET_SEED>,  "\n" );
-print( CONFIG <PACKAGE_SEED>, "\n" );
+print( CONFIG <COMMON_SEED>,    "\n" );
+print( CONFIG <FEATURES_SEED>,  "\n" );
+print( CONFIG <KERNEL_SEED>,    "\n" );
+print( CONFIG <TARGET_SEED>,    "\n" );
+print( CONFIG <SUBTARGET_SEED>, "\n" );
+print( CONFIG <PACKAGE_SEED>,   "\n" );
 close(CONFIG);
 
 print( KCONFIG <KERNEL_COMMON_SEED>, "\n" );
