1ee02876545f774431a7d551d128602f7efbbc82
Oever González <software@notengobattery.com>
2024-04-09 19:04:47 -0600

    ipq40xx: add new devices and customize build
    Signed-off-by: Oever González <software@notengobattery.com>

--- target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ipq40xx/base-files/lib/upgrade/platform.sh
@@ -169,6 +169,7 @@ platform_do_upgrade() {
 	linksys,ea6350v3 |\
 	linksys,ea8300 |\
 	linksys,mr8300 |\
+	linksys,mr9000 |\
 	linksys,whw01 |\
 	linksys,whw03v2)
 		platform_do_upgrade_linksys "$1"
--- target/linux/ipq40xx/image/generic.mk
+++ target/linux/ipq40xx/image/generic.mk
@@ -711,6 +711,23 @@ define Device/linksys_mr8300
 endef
 TARGET_DEVICES += linksys_mr8300
 
+define Device/linksys_mr9000
+	$(call Device/FitzImage)
+	$(call Device/kernel-size-6350-8300)
+	DEVICE_VENDOR := Linksys
+	DEVICE_MODEL := MR9000
+	SOC := qcom-ipq4019
+	KERNEL_SIZE := 5120k
+	IMAGE_SIZE := 84992k
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	UBINIZE_OPTS := -E 5    # EOD marks to "hide" factory sig at EOF
+	IMAGES += factory.bin
+	IMAGE/factory.bin  := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | linksys-image type=MR9000
+	DEVICE_PACKAGES := ath10k-firmware-qca9888-ct kmod-usb-ledtrig-usbport
+endef
+TARGET_DEVICES += linksys_mr9000
+
 define Device/linksys_whw03v2
 	$(call Device/FitzImage)
 	DEVICE_VENDOR := Linksys
